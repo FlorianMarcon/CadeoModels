dae82c2493763b110a657ecede574b6288dca09a
FlorianMarcon <florian.marcon@epitech.eu>
2020-12-18 15:59:30 +0100

    Brand models
    [+] Create IBrand and IBrandCreate interfaces

---
+++ src/Brand/Brand.ts
@@ -0,0 +1,21 @@
+/**
+ * Interface representing brand informations
+ */
+export default interface IBrand {
+
+    _id     :    number; /** Id of the brand */
+
+    name    :   string; /** Name of the brand */
+
+    description?:    string; /** Description of the brand. Can be undefined */
+}
+
+/**
+ * Interface used to create a brand
+ */
+export interface IBrandCreate {
+
+    name            :   string; /** Name of the brand */
+
+    description?    :    string; /** Description of the brand. Can be null */
+}
